ed368193ff8b481d5cd7af35534a50a610531ff8
IshanArya <ishan.arya130@gmail.com>
2018-08-28 00:53:10 -0400

    init commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+	let nameStr = "Hi, I'm Ishan";
+
+	for (var i = 0; i < nameStr.length; i++) {
+	    setTimeout(function() {
+	        document.getElementById('name').textContent = nameStr.substr(0, document.getElementById('name').textContent.length + 1);
+	    }, 200 + i * 75);
+	}
+}
