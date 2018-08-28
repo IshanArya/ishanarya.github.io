aee3bf670b3a3b6cf52accc165b67967a133a88b
IshanArya <ishan.arya130@gmail.com>
2018-08-28 01:02:07 -0400

    Changed time

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ window.onload = function() {
 	for (var i = 0; i < nameStr.length; i++) {
 	    setTimeout(function() {
 	        document.getElementById('name').textContent = nameStr.substr(0, document.getElementById('name').textContent.length + 1);
-	    }, 200 + i * 75);
+	    }, 200 + i * 90);
 	}
 }
